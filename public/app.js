6b536c9574c5154956c56386b87ce4b899251b3d
Ruiz <ruiz.akpan@gmail.com>
2014-04-05 16:37:47 -0500

    Saving...

--- public/app.js
+++ public/app.js
@@ -1,40 +1,30 @@
-var IO = {
-    // This called when the page is loaded. Connect to the socket.
-    init : function() {
-        IO.socket = io.connect(),
-        IO.bindEvents();
-    },
+var ioSocket = io.connect();
 
-    bindEvents : function() {
-        IO.socket.on('connected', App.onConnected);
-        IO.socket.on('newGameCreated', IO.onNewGameCreated );
-        IO.socket.on('updateGameList', IO.updateGameList );
 
-        //TODO
-        // IO.socket.on('gameJoined', IO.onGameJoined );
-        IO.socket.on('showError', IO.showError );
-    },
+// Binding events
+ioSocket.on('connected', onConnected);
+ioSocket.on('newGameCreated', onNewGameCreated );
+ioSocket.on('updateGameList', updateGameList );
 
-    // Set socket ID (session)
-    onConnected : function() {
-        // Cache a copy of the client's socket.IO session ID on the App
-        App.sid = IO.socket.socket.sessionid;
-        // console.log(data.message);
-    },
+//TODO
+// ioSocket.on('gameJoined', IO.onGameJoined );
+ioSocket.on('showError', showError );
 
-    onNewGameCreated : function(game) {
-        // App.Game.gameInit(data);
-    },
+function onConnected() {
+    // Cache a copy of the client's socket.IO session ID on the App
+    App.sid = ioSocket.socket.sessionid;
+    // console.log(data.message);
+    
+}
 
-    updateGameList : function(games){ // data = games
-        App.Game.updateGameList(games);
-    },
+function updateGameList() {
+    App.Game.updateGameList(games);
+}
 
-    showError : function(data) {
-        console.log('err: ' + data);
-        alert(data);
-    }
-};
+function showError(data) {
+    console.log('err: ' + data);
+    alert(data);
+}
 
 var App = {
     gid: 0, // game ID
@@ -86,7 +76,7 @@ var App = {
                 console.log('Game name: ' + gameName);
                 console.log('Clicked "Create a game"');
 
-                IO.socket.emit('createNewGame', gameName);
+                ioSocket.emit('createNewGame', gameName);
         },
 
         onJoinClick: function() {
@@ -96,7 +86,7 @@ var App = {
                 console.log('Game name: ' + gameName);
                 console.log('Clicked "Join a game"');
 
-                IO.socket.emit('joinGame', gameName);
+                ioSocket.emit('joinGame', gameName);
         },
     },
 
@@ -106,5 +96,4 @@ var App = {
     }
 }
 
-IO.init();
 App.init();
