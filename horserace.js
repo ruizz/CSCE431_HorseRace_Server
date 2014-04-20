f946c8e3a8841991351c183afbc61470f59d4f55
Jinsoo Kim <kim10987@dhcp-10-202-117-98.tamulink.tamu.edu>
2014-04-20 01:00:40 -0500

    -
    -

--- horserace.js
+++ horserace.js
@@ -128,6 +128,9 @@ exports.init = function(sio, socket){
         // Havent tested yet
         games[data.gameName].horseBetValues[data.horseNumber] += data.money;
         games[data.gameName].userMoney[data.email][data.horseNumber] += data.money;
-        io.sockets.in(data.gameName).emit(games[data.gameName].horseBetValues);
+        io.sockets.in(data.gameName).emit('updateMoneyOnHorses', {
+            gameName: data.gameName,
+            money: games[data.gameName].horseBetValues
+        });
     });
 };
