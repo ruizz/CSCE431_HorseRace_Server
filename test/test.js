9f8f5ddb20841444fc60ea075b0eac464564f86a
Zachary Partal <zpartal@gmail.com>
2014-04-22 13:03:43 -0500

    some tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,10 @@ var assert = require('assert');
 var mocha = new Mocha();
 var server = require('../server');
 var request = require('request');
+var game = require('../game');
+var horserace = require('../horserace');
+var socketio = require('socket.io');
+
 mocha.reporter('dot');
 
 var runner = mocha.run(function(){} );
@@ -18,6 +22,71 @@ describe('Test Mocha', function(){
 	});
 });
 
+describe('game.js Tests', function() {
+	describe('game', function() {
+		var sio = socketio.listen(5000);
+		var testGame = new game("test", sio);
+
+		it("should be a game named test", function() {			
+			testGame.gameName.should.equal('test');
+		});
+
+		it("should be totalbets equal to 10", function() {
+			testGame.horseBetValues = [1,1,1,1,1,1,1,3];
+			testGame.updateTotal();
+			testGame.totalBets.should.equal(10);
+		});
+	});
+
+	describe('game2', function() {
+		var sio = socketio.listen(5000);
+		var testGame = new game("test", sio);
+		testGame.players = {
+							    "devan" :12345,
+							    "ruiz" : 12345,
+							    "zach" : 12345,
+							    "eliott" : 12345
+							};
+		testGame.totalBets = 95;
+		testGame.horseBetValues = [11,25,13,4,7,11,14,10];
+		testGame.userMoney = {
+		    "devan" : [0,18,0,0,0,1,4,1],
+		    "ruiz" : [1,4,3,3,4,3,3,3],
+		    "zach" : [6,1,7,1,2,2,3,2],
+		    "eliott" : [4,2,3,0,1,5,4,4]
+		}
+		testGame.winningHorses = [2,3,7];
+
+		it("calculate winnings function", function() {			
+			testGame.calculateWinnings().should.containEql({ devan: '3.17', ruiz: '40.56', zach: '31.30', eliott: '19.97' });
+		});
+
+		it("game moves", function() {			
+			testGame.generateMoves().should.have.length(8);
+			testGame.moves.should.have.length(8);
+		});
+
+
+	});
+});
+
+describe('Horserace.js Tests', function(){
+	var sio = socketio.listen(5000);
+	var hr = null;
+
+	it('Should init the horserace', function(){
+		sio.sockets.on('connection', function (socket) {
+	    	hr = horserace.init(sio, socket);
+		});
+	});
+
+	it('Should tell the client it is connected', function(){
+
+	});
+
+});
+
+
 describe('Server.js Tests', function(){
 	
 	it('Should accept an incoming http connection and respond', function(){
@@ -29,16 +98,4 @@ describe('Server.js Tests', function(){
 
 });
 
-// Todo
-// describe('Horserace.js Tests', function(){
 
-// 	it('Should init the horserace', function(){
-
-
-// 	});
-
-// 	it('Should tell the client it is connected', function(){
-
-// 	});
-
-// });
